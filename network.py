e51f01f35d4144eda41510847324b1bdd0d45131
peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2025-07-26 21:25:06 +0200

    Add .gitignore

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Network:
     def __init__(self, input_n: int, hiddens_n: list, output_n: int, sigma: float = 0.001):
-        self._input_neurons_num: int = input_n
+        self._input_neurons: int = input_n
         self._hidden_neurons: list = hiddens_n
         self._output_neurons: int = output_n
         self._sigma: float = sigma
@@ -12,7 +12,7 @@ class Network:
         self._init_params()
 
     def _init_params(self) -> None:
-        prev_conn_num: int = self._input_neurons_num
+        prev_conn_num: int = self._input_neurons
         for curr_hidden in self._hidden_neurons:
             # Baiases array for each curr_hidden neurons (one bias for each neuron)
             self._baiases.append(self._sigma * np.random.normal(size=[curr_hidden, 1]))
@@ -25,7 +25,7 @@ class Network:
         self._weights.append(self._sigma * np.random.normal(size=[prev_conn_num, self._output_neurons]))
 
     def _get_info(self) -> None:
-        print(f"Network [{self._input_neurons_num} x {self._hidden_neurons} x {self._output_neurons}]")
+        print(f"Network [{self._input_neurons} x {self._hidden_neurons} x {self._output_neurons}]")
         print(f"baiases: {self._baiases}")
         print(f"weights: {self._weights}")
 
